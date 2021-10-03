4061dc5f9c461f913134fe17292b506aa6c596be
SMOLKEYS <jiroshimonis@gmail.com>
2021-10-03 14:04:33 +0800

    the l

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    app: {
+        px: '.',
+        token: 'ODgxODkyOTA2NjU3NTI5ODc3.YSzcxQ.jP1lYuNfJjFowo7BU1HZJ2wFcJc',
+        playing: 'the large l'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        loopMessage: false,
+        discordPlayer: {}
+    }
+};
---
+++ main.js
@@ -0,0 +1,31 @@
+const { Player } = require('discord-player');
+const { Client, Intents } = require('discord.js');
+
+global.client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES
+    ],
+    disableMentions: 'everyone',
+});
+
+client.config = require('./config');
+
+global.player = new Player(client, client.config.opt.discordPlayer);
+
+require('./src/loader');
+require('./src/events');
+/*
+client.on("messageCreate", (messsge) => {
+	if(message.content == "terminate"){
+		if(message.author == "691650272166019164"){
+			message.channel.send("Bot killed.").then(message => process.exit())
+		} else {
+			message.channel.send("no")
+		}
+	}
+});
+*/
+client.login(client.config.app.token);
